b3e616d792ba5503fc902137d85f2b3e8fc30d3c
nidhugopal16 <ngopal3@my.bcit.ca>
2019-10-18 11:37:04 -0700

    db connection added

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace ConsoleApp2
 {
     class Program
     {
+        class SqlHelper
+        {
+            public static SqlDataReader ExcecuteReader(String connString, String commandText)
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    return reader;
+                }
+                return null;
+            }
+                   
+        }
         static void Main(string[] args)
         {
             String connString = "Data Source=.; Initial Catalog=pubs; Integrated Security=true;";
             String commandText = "SELECT au_fname, au_lname FROM authors;";
+
+            using (SqlDataReader reader = SqlHelper.ExcecuteReader(connString, commandText))
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("Item");
+                }
+            }
+
             Console.WriteLine("Hello World!");
             Console.Read();
         }
     }
 }
+
+
