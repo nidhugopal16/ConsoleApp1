c2f67488889be6f134292222fb3b6067dccdaeea
nidhugopal16 <ngopal3@my.bcit.ca>
2019-10-18 13:15:04 -0700

    Getting authors through entity framework scafold database

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using ConsoleApp2.Models;
+using System;
 using System.Data.SqlClient;
 
 namespace ConsoleApp2
 {
     class Program
     {
+
+        
         class SqlHelper
         {
+           
             public static SqlDataReader ExcecuteReader(String connString, String commandText)
             {
                 SqlConnection conn = new SqlConnection(connString);
                 using (SqlCommand cmd = new SqlCommand(commandText, conn))
                 {
+                    Console.WriteLine("\n Getting by manually making connecting to sql\n");
                     conn.Open();
                     SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
                     return reader;
@@ -30,6 +35,18 @@ namespace ConsoleApp2
                 while (reader.Read())
                 {
                     Console.WriteLine(reader["au_fname"].ToString() + " " + reader["au_lname"].ToString());
+                }
+                Console.WriteLine("\n Getting authors through entity framework scafold database \n");
+
+                var db = new pubsContext();
+                foreach (Authors author in db.Authors)
+                {
+                    Console.WriteLine(author.AuFname + " " + author.AuLname);
+                }
+                Console.WriteLine("\n Getting Title through scafold database \n");
+                foreach (Titles title in db.Titles)
+                {
+                    Console.WriteLine(title.Title);
                 }
             }
 
