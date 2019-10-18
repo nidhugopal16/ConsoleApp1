72e93c5dbbe997266ab341c2cc2051fc836eda69
nidhugopal16 <ngopal3@my.bcit.ca>
2019-10-18 10:25:53 -0700

    my first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String connString = "Data Source=.; Initial Catalog=pubs; Integrated Security=true;";
+            String commandText = "SELECT au_fname, au_lname FROM authors;";
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
